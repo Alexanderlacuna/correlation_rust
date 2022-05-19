1a7753b350e862b844d3c2fa27766e52f499efc7
Alexander <alexanderkabua@gmail.com>
2022-05-19 14:10:45 +0300

    code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,15 @@
 use std::env;
-use correlation_rust::parser;
-use correlation_rust::correlations;
+use correlation_rust::analysis::Analysis;
 fn main () {
-
-
-    let json_file = match env::args().nth(1){
+    match env::args().nth(1){
         Some(tmp_file) => {
-            let results = parser::JsonData::new(&tmp_file);
-
-
-            let parsed_x_vals:Vec<f64> = results.x_vals.split(",").map(|f|f.trim().parse::<f64>().unwrap()).collect();
-      
-
-
-            let computation = correlations::Compute::new(
-                ',',
-                "pearson",
-                &results.file_path,
-        &parsed_x_vals
-
-            ).compute();
-
-            //println!("{:?}",computation);
-
+         
+            match Analysis::compute(&tmp_file){
+                Ok(results) => println!("{:?}",results),
+                Err(err) => println!("{:?}",err)
+            }
         }
         None => panic!("expected an arugment for the json file")
     };
-
-
-
-
 
 }
