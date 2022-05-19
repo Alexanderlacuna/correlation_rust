4adac6e6363e850c37e5f8f3e8d4424fffd8511d
Alexander <alexanderkabua@gmail.com>
2022-05-19 14:11:06 +0300

    add entry point module

---
+++ src/analysis.rs
@@ -0,0 +1,38 @@
+// entry point 
+use crate::parser;
+use crate::correlations;
+pub struct Analysis;
+impl Analysis {
+    pub fn compute(tmp_file:&str) -> std::io::Result<Vec<(f64,f64)>>{
+
+        let json_results = parser::JsonData::new(tmp_file);
+
+        //above should return  a results 
+
+        let x_vals:Vec<f64>= json_results.x_vals.split(",").map(|f|f.trim().parse::<f64>().unwrap()).collect();
+
+        let computation = correlations::Compute::new(
+            ',',
+            "pearson",
+            &json_results.file_path,
+    &x_vals
+
+        ).compute();
+
+        return Ok(computation);
+
+
+    }
+}
+
+
+
+#[cfg(test)]
+
+mod tests {
+#[test]
+fn test_sum(){
+    assert_eq!(1,5)
+}
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod reader;
 pub mod correlations;
 pub mod parser;
+pub mod analysis;
