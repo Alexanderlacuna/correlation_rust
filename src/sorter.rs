17b81df6e833957839fbd5359565c9aa73b76aaf
Alexander <alexanderkabua@gmail.com>
2022-05-27 00:51:06 +0300

    function to write to file

--- src/sorter.rs
+++ src/sorter.rs
@@ -1,14 +1,17 @@
 // implementation of extern sorter with rust
 
 //https://betterprogramming.pub/how-to-sort-a-20g-file-in-rust-12abfffbd92b
-use std::io::{Write,Read};
+use std::io::{Write,Read,BufWriter};
 use std::fs::File;
  use extsort::*;
 
+
+const BUFFER_CAPACITY: usize = 4_000_000_000;
 //struct corr_results
 
 #[derive(Debug, PartialEq, PartialOrd)]
 struct CorrResults(String,f64,f64);
+
 
 
 impl  Sortable for  CorrResults {
@@ -41,7 +44,17 @@ fn ext_sorter (unsorted_vec:Vec<CorrResults>,file_name:&str,n_top:usize)-> std::
 
     //corr_results.sort_by(|a,b|b.1.abs().partial_cmp(&a.1.abs()).unwrap());
 
+}
 
 
+pub fn write_to_file(filename: String, v: Vec<(String,f64,f64)>) {
+    let mut buffer = BufWriter::with_capacity(BUFFER_CAPACITY, File::create(&filename).unwrap());
+    for x in v.iter() {
+        writeln!(buffer, "{:?}", x).unwrap();
+    }
+    buffer.flush().unwrap();
+}
 
+pub fn create_large_file(filename: &str) {
+    File::create(filename).unwrap();
 }
