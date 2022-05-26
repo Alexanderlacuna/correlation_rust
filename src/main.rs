c5efda5ef051e3c0ee0cd48be67866855f47c678
Alexander <alexanderkabua@gmail.com>
2022-05-26 22:00:24 +0300

    debug code for elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 use correlation_rust::analysis::Analysis;
 fn main () {
+
+    use std::time::Instant;
+    let now = Instant::now();
+
     match env::args().nth(1){
         Some(tmp_file) => {
          
@@ -11,5 +15,9 @@ fn main () {
         }
         None => panic!("expected an arugment for the json file")
     };
+
+
+    let elapsed = now.elapsed();
+    println!("Elapsed:>>>>>>>>>>>>>>>>>>>>{:.2?}", elapsed);
 
 }
